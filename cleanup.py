9f300805529b609e27f0f20c59851a16fa68f75a
Nils F. Karlsson <nils@tibiahof.com>
2015-01-24 23:00:13 +0100

    cleanup script to remove .pyc-files recursively (useful for windows users)

---
+++ cleanup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+"""
+    python script that cleans up by removing .pyc-files recursively
+    inspired by solution found at:
+    https://djangosnippets.org/snippets/899/
+
+    to run, simply type:
+    python cleanup.py
+"""
+def pyc_cleanup(directory, path):
+    deleted = 0
+    for filename in directory:
+        if filename[-3:] == 'pyc':
+            print(' - ' + filename)
+            os.remove(path+os.sep+filename)
+            deleted += 1
+        elif os.path.isdir(path+os.sep+filename):
+            deleted += pyc_cleanup(os.listdir(path+os.sep+filename), path+os.sep+filename)
+    return deleted
+
+if __name__ == '__main__':
+    directory = os.listdir('.')
+    print("Delete pyc files recursively in directory: " + os.getcwd())
+    deleted = pyc_cleanup(directory, ".")
+    print "Deleted", deleted, ".pyc files"
