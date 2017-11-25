4b6ba3af83cddf12b5bfdd19ad00ed095a169b5a
Nils F. Karlsson <nils@tibiahof.com>
2017-11-25 19:54:56 +0100

    add encoding + charset settings to config files (defaults to utf-8)

--- py_privatekonomi/core/db.py
+++ py_privatekonomi/core/db.py
@@ -12,20 +12,29 @@ from py_privatekonomi.utilities.common import (singleton, is_dict, is_Struct, as
 class DB(object):
     def connect(self, db_config):
         if is_dict(db_config):
+            if 'charset' not in db_config:
+                db_config['query'] = None
+            else:
+                db_config['query'] = {
+                    'charset' : db_config['charset']
+                }
+            if 'encoding' not in db_config:
+                db_config['encoding'] = 'utf-8'
+            else:
+                if db_config['encoding'] == 'utf8':
+                    db_config['encoding'] = 'utf-8'
             db_config = as_obj(db_config)
-        else:
-            if not is_Struct(db_config):
-                raise Exception("db_config must be either dict or common.Struct: %s" % (repr(db_config)))
+        elif not is_Struct(db_config):
+            raise Exception("db_config must be either dict or common.Struct: %s" % (repr(db_config)))
         engine_url = url.URL(
             drivername=db_config.engine,
             host=db_config.host,
             port=db_config.port,
             username=db_config.username,
             password=db_config.password,
-            database=db_config.database,
-            query={ 'charset' : 'utf8' }
+            database=db_config.database
         )
-        self.__engine = create_engine(engine_url, encoding='utf-8')
+        self.__engine = create_engine(engine_url)
         self.__connection = self.__engine.connect()
         self.__config = db_config
         self.__connected = True
