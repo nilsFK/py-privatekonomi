b85c0b1245614dba613f61f4c4f44297f7908d3b
Nils F. Karlsson <nils@tibiahof.com>
2019-11-21 17:34:11 +0100

    #26: Swedbank csv update fix

--- py_privatekonomi/core/formatters/swedbank_formatter.py
+++ py_privatekonomi/core/formatters/swedbank_formatter.py
@@ -14,39 +14,39 @@ class SwedbankFormatter(AccountFormatter):
     @EconomyMapper("Account", "account_code")
     def format_account_account_code(self, content, subformatter):
         """ Clearing-nummer """
-        return content.strip()
+        return content.strip().strip('"')
 
     @EconomyMapper("Account", "account_number")
     def format_account_account_number(self, content, subformatter):
         """ Kontonummer / Kontonr """
-        return content.strip()
+        return content.strip().strip('"')
 
     @EconomyMapper("Account", "name")
     def format_account_name(self, content, subformatter):
         """ Kontonamn """
-        return content.strip()
+        return content.strip().strip('"')
 
     @EconomyMapper("TransactionType", "name")
     def format_transaction_type_name(self, content, subformatter):
         """ Kontohändelse """
-        return content.strip()
+        return content.strip().strip('"')
 
     @EconomyMapper("Transaction", "accounting_date")
     def format_transaction_accounting_date(self, content, subformatter):
         """ Bokföringsdatum """
-        content = content.strip()
+        content = content.strip().strip('"')
         return super(SwedbankFormatter, self)._format_date(content, None)
 
     @EconomyMapper("Transaction", "transaction_date")
     def format_transaction_transaction_date(self, content, subformatter):
         """ Transaktionsdatum """
-        content = content.strip()
+        content = content.strip().strip('"')
         return super(SwedbankFormatter, self)._format_date(content, None)
 
     @EconomyMapper("Transaction", "reference")
     def format_transaction_reference(self, content, subformatter):
         """ Referens """
-        return content.strip()
+        return content.strip().strip('"')
 
     @EconomyMapper("Transaction", "amount")
     def format_transaction_amount(self, content, subformatter):
@@ -56,7 +56,7 @@ class SwedbankFormatter(AccountFormatter):
     @EconomyMapper("Currency", "code")
     def format_currency_code(self, content, subformatter):
         """ Typ av valuta """
-        return content.strip()
+        return content.strip().strip('"')
 
     @EconomyMapper("Account", "current_balance")
     def format_account_current_balance(self, content, subformatter):
